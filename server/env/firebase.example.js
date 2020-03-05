12dca98c829a199af0924ab65eed68ba9d622a5c
psfonseka <psfonseka@gmail.com>
2020-03-05 12:41:45 -0500

    initialized firebase for authentication

---
+++ server/env/firebase.example.js
@@ -0,0 +1,13 @@
+//Follow this pattern with your own firebase info on firebase.js
+const firebaseConfig = {
+  apiKey: "api-key",
+  authDomain: "project-id.firebaseapp.com",
+  databaseURL: "https://project-id.firebaseio.com",
+  projectId: "project-id",
+  storageBucket: "project-id.appspot.com",
+  messagingSenderId: "sender-id",
+  appId: "app-id",
+  measurementId: "G-measurement-id",
+};
+
+module.exports = firebaseConfig;
--- server/index.js
+++ server/index.js
@@ -1,3 +1,16 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+const firebase = require("firebase/app");
+const firebaseConfig = require("./env/firebase");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Express App
 const express = require('express')
 const app = express()
 const port = 3000
